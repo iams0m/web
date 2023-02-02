60ba5815dbc91ea323de3888eb539adc2e8cfce0
iams0m <105639531+iams0m@users.noreply.github.com>
2023-02-02 17:36:54 +0900

    Add files via upload

---
+++ day04/out2.js
@@ -0,0 +1,9 @@
+function 여행하다() {
+    console.log("1. 여행 장소를 정한다.")
+    console.log("2. 여행 계획을 세운다.")
+   }
+function 쉬다() {
+    console.log("1. 샤워를 한다.")
+    console.log("2. 침대에 눕는다.")
+    console.log("3. 넷플릭스를 시청한다.")
+   }
